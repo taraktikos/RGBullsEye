60ace56325069c35100c2ab6ac0ded09bc6791fd
Taras S <tarassuhovenko@gmail.com>
2020-03-28 19:34:01 +0200

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Taras on 28.03.2020.
+//  Copyright © 2020 Taras. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
