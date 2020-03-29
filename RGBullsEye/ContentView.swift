127af4674509e750bd2e41a298da3be8d2eb552a
Taras S <tarassuhovenko@gmail.com>
2020-03-29 19:32:53 +0300

    changed view

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                VStack {
+                    /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+                    Text("Match this color")
+                }
+                VStack {
+                    /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+                    Text("R: 127 G: 127 B: 127")
+                }
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit Me!")
+            }
+            Slider(value: .constant(0.5))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 548, height: 320))
     }
 }
