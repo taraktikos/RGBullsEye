e4beb67f01c0dd6ee569dff1468d664de3e9d403
Taras S <tarassuhovenko@gmail.com>
2020-04-01 22:06:45 +0300

    added score button

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,28 +9,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double .random(in: 0..<1)
+    let gTarget = Double .random(in: 0..<1)
+    let bTarget = Double .random(in: 0..<1)
+    
+    @State
+    var rGuess: Double
+    @State
+    var gGuess: Double
+    @State
+    var bGuess: Double
+    
+    @State
+    var showAlert = false
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100.0 + 0.5)
+    }
+    
     var body: some View {
         VStack {
             HStack {
                 VStack {
-                    /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
                     Text("Match this color")
                 }
                 VStack {
-                    /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
-                    Text("R: 127 G: 127 B: 127")
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))"
+                        + " G: \(Int(gGuess * 255.0))"
+                        + " B: \(Int(bGuess * 255.0))")
                 }
             }
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: { self.showAlert = true }) {
                 Text("Hit Me!")
-            }
-            Slider(value: .constant(0.5))
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+            }.padding()
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().previewLayout(.fixed(width: 548, height: 320))
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 548, height: 320))
+    }
+}
+
+struct ColorSlider: View {
+    @Binding
+    var value: Double
+    
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }.padding(.horizontal)
     }
 }
